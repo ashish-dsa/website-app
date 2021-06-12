3e532c2263102bf9edf9223aeb90ff91ed563faa
Ashish D'sa <ashd621@gmail.com>
2021-06-13 02:26:27 +0530

    Updated src/navigation/constants.ts

---
+++ src/navigation/constants.ts
@@ -0,0 +1,10 @@
+export enum ROUTES {
+  ExampleScreen1 = "ExampleScreen1",
+  ExampleScreen2 = "ExampleScreen2",
+  Home = "Home",
+  About = "About",
+  Portfolio = "Portfolio",
+  Services = "Services",
+  Skills = "Skills",
+  Testimonials = "Testimonials",
+}
