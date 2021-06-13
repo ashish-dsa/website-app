442a715ef9e6c346477e83c881cd17970e197133
Ashish D'sa <ashd621@gmail.com>
2021-06-14 01:40:31 +0530

    Updated src/screens/Testimonials/testimonialCollection.ts

---
+++ src/screens/Testimonials/testimonialCollection.ts
@@ -0,0 +1,29 @@
+export const testimonialCollection = [
+  {
+    name: "Apoorv Karkare",
+    info: "Software Engineer at Walmart",
+    relation: "Ashish worked with Apoorv in the same group.",
+    recommendation:
+      "Ashish D'sa in my opinion is a very quick learner. He always keeps exploring new technologies and implement the same in his work. He is always ready to share his knowledge whenever it's asked, which shows his team spirit.",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQGluPRskh95xw/profile-displayphoto-shrink_100_100/0/1516796667658?e=1628726400&v=beta&t=u0MEwUlPX7EDL_E3rPLjNc7JSZXdxKoqySL1xJgPTng",
+  },
+  {
+    name: "Chandramouli Godhandaraman",
+    info: "VP Engineering at 314e Corporation",
+    relation: "Chandramouli was senior to Ashish but didn’t manage directly.",
+    recommendation:
+      "Ashish is one of the brightest programmers that I have come across. During my stay at Hiver, I observed how he took any challenge offered to him, made significant contributions in the area of code quality management with minimal guidance and work as a critical part of a team building mobile app grounds up. He will be a great asset to any team that he is a part of both technically and morally.",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C5603AQFTfssmbmk0Ag/profile-displayphoto-shrink_100_100/0/1603095447031?e=1628726400&v=beta&t=HtVa206STeFOCDluE1byu2oAgXLu-bmX4VQf-DGcJgY",
+  },
+  {
+    name: "Thompson Naidu",
+    info: "Software Engineer at Quantiphi",
+    relation: "Ashish worked with Thompson in the same group",
+    recommendation:
+      "Ashish D'sa was a fantastic person to work with, and is not only a multi-skilled and insightful colleague, but also an inspiring strategist. Very good person. Great employee with a very strong problem solving skills. Ashish is an asset to any company.",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQEbqGZ7SE1VRw/profile-displayphoto-shrink_100_100/0/1603357082423?e=1628726400&v=beta&t=hM7Bp3IR0wOdWsxW0UHQ9KhQRO375R3UUEapwurYbf4",
+  },
+];
