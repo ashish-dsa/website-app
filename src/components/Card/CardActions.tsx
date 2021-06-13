c2dda8b349efc7fd18b48d7484dc6cbcf4b089fe
Ashish D'sa <ashd621@gmail.com>
2021-06-14 01:40:30 +0530

    Updated src/components/Card/CardActions.tsx

--- src/components/Card/CardActions.tsx
+++
@@ -1,8 +0,0 @@
-import React, {ReactElement} from "react";
-import {Card as PaperCard} from "react-native-paper";
-
-type CardActions = React.ComponentProps<typeof PaperCard.Actions>;
-
-export const CardActions: React.FC<CardActions> = (props): ReactElement => {
-  return <PaperCard.Actions {...props} />;
-};
