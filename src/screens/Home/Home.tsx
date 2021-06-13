6c3ea76ae3fa5dfc7929349d2c56882dc49fad59
Ashish D'sa <ashd621@gmail.com>
2021-06-14 01:40:30 +0530

    Updated src/screens/Home/Home.tsx

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,11 @@
 import AnimatedText from "components/AnimatedText";
-import NavigationHeader from "components/NavigationHeader";
 import React, {ReactElement} from "react";
-import {StyleSheet, View} from "react-native";
+import {Dimensions, StyleSheet, View} from "react-native";
 
 interface IHomeProps {}
 const Home: React.FC<IHomeProps> = (props): ReactElement => {
   return (
     <View style={styles.container}>
-      <NavigationHeader />
       <AnimatedText />
     </View>
   );
@@ -16,6 +14,6 @@ const Home: React.FC<IHomeProps> = (props): ReactElement => {
 export default Home;
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
+    height: Dimensions.get("window").height,
   },
 });
