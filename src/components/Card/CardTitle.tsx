4f4f2cc54f758e09ec16a0007d95083c81b0766d
Ashish D'sa <ashd621@gmail.com>
2021-06-14 01:40:30 +0530

    Updated src/components/Card/CardTitle.tsx

--- src/components/Card/CardTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React, {ReactElement} from "react";
-import {Card as PaperCard} from "react-native-paper";
-
-type CardTitle = React.ComponentProps<typeof PaperCard.Title>;
-
-export const CardTitle: React.FC<CardTitle> = (props): ReactElement => {
-  return <PaperCard.Title {...props} />;
-};
