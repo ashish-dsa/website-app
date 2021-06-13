61553bd8b330d8382e7f0cb93c012b2727e437bd
Ashish D'sa <ashd621@gmail.com>
2021-06-14 01:40:30 +0530

    Updated src/components/NavigationHeader/NavigationHeader.tsx

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,23 +1,53 @@
-import {useNavigation} from "@react-navigation/native";
+import {Theme, useNavigation} from "@react-navigation/native";
+import NavigationButton from "components/NavigationButton";
 import {ROUTES} from "navigation/constants";
-import React, {ReactElement} from "react";
-import {StyleSheet, View} from "react-native";
-import {Appbar} from "react-native-paper";
+import React, {ReactElement, useState} from "react";
+import {Image, StyleSheet, View} from "react-native";
+import {Switch, TouchableRipple} from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import {CombinedDarkTheme, CombinedDefaultTheme} from "services/theming";
 
-interface INavigationHeaderProps {}
-const NavigationHeader: React.FC<INavigationHeaderProps> = (): ReactElement => {
+interface INavigationHeaderProps {
+  theme: Theme;
+  setTheme: Function;
+}
+const PLAY_BUTTON_IMAGE =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/320px-Google_Play_Store_badge_EN.svg.png";
+
+const NavigationHeader: React.FC<INavigationHeaderProps> = ({theme, setTheme}): ReactElement => {
   const navigation = useNavigation();
+  const [darkTheme, setDarkTheme] = useState(theme === CombinedDarkTheme);
 
+  const switchTheme = () => {
+    if (darkTheme) {
+      setTheme(CombinedDefaultTheme);
+      setDarkTheme(false);
+    } else {
+      setTheme(CombinedDarkTheme);
+      setDarkTheme(true);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Appbar.Header>
-        <Appbar.Content title="Home" subtitle="" onPress={() => navigation.navigate(ROUTES.Home)} />
-        <Appbar.Content title="Portfolio" subtitle="" onPress={() => navigation.navigate(ROUTES.Portfolio)} />
-        <Appbar.Content title="About" subtitle="" onPress={() => navigation.navigate(ROUTES.About)} />
-        <Appbar.Content title="Services" subtitle="" onPress={() => navigation.navigate(ROUTES.Services)} />
-        <Appbar.Content title="Skills" subtitle="" onPress={() => navigation.navigate(ROUTES.Skills)} />
-        <Appbar.Content title="Testimonials" subtitle="" onPress={() => navigation.navigate(ROUTES.Testimonials)} />
-      </Appbar.Header>
+      <NavigationButton selected={false} name={ROUTES.Home} navigation={navigation} />
+      <NavigationButton selected={false} name={ROUTES.Skills} navigation={navigation} />
+      <NavigationButton selected={false} name={ROUTES.Works} navigation={navigation} />
+      <NavigationButton selected={false} name={ROUTES.About} navigation={navigation} />
+      <NavigationButton selected={false} name={ROUTES.Testimonials} navigation={navigation} />
+      <View style={styles.theme}>
+        <Icon name="lightbulb-on-outline" size={22} color={theme.colors.text} />
+        <Switch style={styles.switch} value={darkTheme} onValueChange={switchTheme} />
+        <Icon name="lightbulb-off-outline" size={22} color={theme.colors.text} />
+      </View>
+      <TouchableRipple style={styles.image}>
+        <Image
+          style={styles.playButton}
+          resizeMode={"cover"}
+          source={{
+            uri: PLAY_BUTTON_IMAGE,
+          }}
+        />
+      </TouchableRipple>
     </View>
   );
 };
@@ -25,5 +55,33 @@ const NavigationHeader: React.FC<INavigationHeaderProps> = (): ReactElement => {
 export default NavigationHeader;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: "row",
+    marginLeft: "10%",
+    marginRight: "35%",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  theme: {
+    paddingTop: "3%",
+    paddingLeft: "4%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  switch: {
+    height: 15,
+    marginHorizontal: 10,
+  },
+  playButton: {
+    flex: 1,
+    height: undefined,
+    width: undefined,
+    resizeMode: "contain",
+  },
+  image: {
+    height: "80%",
+    width: "20%",
+    paddingLeft: "5%",
+    marginTop: "3%",
+  },
 });
